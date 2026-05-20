74a50d363311af65c2c5378a90da54b26f990e50
Evelinmq <20243ds170@utez.edu.mx>
2026-05-20 11:29:56 -0600

    Initial commit

---
+++ src/main/java/mx/edu/utez/JuventudxTemixco/JuventudxTemixcoApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.JuventudxTemixco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuventudxTemixcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuventudxTemixcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/utez/JuventudxTemixco/JuventudxTemixcoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.JuventudxTemixco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuventudxTemixcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
