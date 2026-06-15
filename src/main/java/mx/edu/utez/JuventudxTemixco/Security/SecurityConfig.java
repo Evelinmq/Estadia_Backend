094c0dc7a2a0a0934e926c70e55e72429c197538
Andreaara <20243ds171@utez.edu.mx>
2026-06-15 13:06:44 -0600

    cambios

--- src/main/java/mx/edu/utez/JuventudxTemixco/Security/SecurityConfig.java
+++ src/main/java/mx/edu/utez/JuventudxTemixco/Security/SecurityConfig.java
@@ -104,6 +104,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/afiliados/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/beneficiarios/**").permitAll()
                         .requestMatchers(HttpMethod.GET,  "/api/beneficiarios/Municipios").permitAll()
+                        .requestMatchers(HttpMethod.GET,  "/api/beneficiarios/verificarCorreo").permitAll()
 
 
                         .requestMatchers(HttpMethod.OPTIONS,
